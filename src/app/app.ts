d2285c142341ae08e13f3a55b6c8664a9a74a0b8
Giacomo Laptop <giacomo.labriola15@gmail.com>
2026-07-24 11:13:32 +0200

    - Fixed various UI bugs - added footer component and updated footer content - fixed hamburger menu pushing content istead of just overlapping it - fixed a bug where the hamburger button would not respond to clicks - removed Herobrine

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationMenu } from './component/navigation-menu/navigation-menu';
-
+import { Footer } from './footer/footer';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavigationMenu],
+  imports: [RouterOutlet, NavigationMenu,Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
